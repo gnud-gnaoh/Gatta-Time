f5207d31cc17a5679b546140adbf566229b7bf9d
ducho <ducho@DESKTOP-4LSE34K>
2023-06-27 22:55:09 +0700

    Manual Page

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Gatta_Time
+namespace donescr
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Gatta_Time
+namespace donescr
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -22,7 +22,7 @@ namespace Gatta_Time
     {
         public MainWindow()
         {
-            InitializeComponent();
+           // InitializeComponent();
         }
     }
 }
