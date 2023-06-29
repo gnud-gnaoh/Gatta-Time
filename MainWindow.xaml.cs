af974ffacb6544b9959fb1e78a7042ebf5e5463e
gnud-gnaoh <hoangdung2.bubu@gmail.com>
2023-06-30 02:08:23 +0700

    Add page navigation.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Gatta_Time
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            _NavigationFrame.Navigate(new MainMenuPage());
+        }
+    }
+}
